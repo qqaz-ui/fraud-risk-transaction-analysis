25f3d7b0960e0986cb0d36746c1cfad0b451f46f
qqaz-ui <qqaz851208@gmail.com>
2026-05-10 19:54:17 +0800

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,102 @@
+INSERT INTO customers VALUES
+('josephhoward@yahoo.com','400-108-5415','yyeiaxpltf82440jnb3v','8.129.104.40','5493 Jones Islands Brownside, CA 51896',2,2,1,'FALSE'),
+
+('evansjeffery@yahoo.com','1-788-091-7546','r0jpm7xaeqqa3kr6mzum','219.173.211.202','356 Elizabeth Inlet Suite 120 Port Joshuabury, NM 37681',3,3,7,'TRUE'),
+
+('andersonwilliam@yahoo.com','024.420.0375','4m7h5ipl1shyavt6vv2r','67b7:3db8:67e0:3bea:b9d0:90c1:2b60:b9f0','8478 Sean Ridges Apt. 441 Davisberg, PR 72250',5,3,2,'FALSE'),
+
+('rubenjuarez@yahoo.com','670.664.8168x94985','slovx60t0i558may4ks0','95de:8565:5a66:792c:26e0:6cfb:7d87:11af','7769 Elizabeth Bridge Apt. 343 Nortonstad, FM 51359',3,3,1,'FALSE'),
+
+('uchen@malone.com','1-981-877-0870','j0pd24k5h8dl2fqu0cz4','196.89.235.192','148 Russell Lodge Apt. 445 Port Jenniferside, TN 60556-0871',7,7,6,'TRUE'),
+
+('robinsoncynthia@dunn.com','+65(5)8997489788','hbww39m1wenbq4ymjhsx','43.166.163.164','65233 Hutchinson Mountains Martinezside, OR 75588',1,1,2,'TRUE'),
+
+('samuel15@saunders-rhodes.com','05045239487','up7afvce5v0omniqlevq','33.67.35.30','3960 Emily Fort Staceytown, AR 21817-3325',2,1,2,'TRUE'),
+
+('johnlowery@gmail.com','044-642-9860','rjb0adai34izqvys4938','212.144.68.190','484 Pamela Pass Lake Jessicaview, WI 12942-9074',6,5,2,'TRUE'),
+
+('jameslandry@rodriguez.com','1-885-168-1310x338','yz4l2bgbvzra5eb5qcan','c899:ff67:6864:4365:def3:3023:655f:5181','USNS Price FPO AE 14661',5,2,1,'FALSE'),
+
+  
+('ubranch@rivera-parker.info','(772)964-8162x6310','6mpx027neyz6sh04n3ue','86.16.65.228','6974 Robertson Common Apt. 902 Port Juanside, ID 80590-9633',0,0,1,'TRUE'),
+
+('bowenwilliam@yahoo.com','(128)248-4091x37789','qmntgod0numz62gzbla7','231.91.253.19','27061 Wright Forks Craigland, MN 59915',6,5,1,'TRUE'),
+
+('ksummers@hotmail.com','1-023-275-4692','2ngkzu1idwxintmxwmgn','184.97.57.11','46383 Matthew Track Apt. 220 Romanburgh, DC 95125',7,5,4,'FALSE'),
+
+('christineklein@wright-boyd.org','897-464-6425x941','j8cp1uydkuyza2eo0mir','4304:521a:e1c3:766b:21f3:ac3:4ab2:5a2','67636 Renee Islands Suite 307 Teresaview, ND 09042',4,4,3,'FALSE'),
+
+('meganwalters@chavez.com','(522)133-8741x643','qsxqbn6qjxkni7f813qn','98.229.160.246','7541 Olson Glens Ballshire, IL 29415',4,2,1,'FALSE'),
+
+('caleb94@sutton.info','07452086668','lixwrlvcr4g5ht600dk9','2efe:240f:cc33:306:8e47:b8d5:b466:f12a','813 Miller Lakes Lake Christina, SD 29778-1298',5,5,2,'FALSE'),
+
+('gonzalesjackson@gmail.com','1-410-373-1228x129','hmkn4nmfc5q5mgify2zw','229.2.225.188','566 Courtney Stream Reedland, NH 84853',4,4,1,'TRUE'),
+
+('dana09@yahoo.com','515.708.2280x105','wb0uylp0rwy01x5j1jzd','166.148.66.158','812 Moss Radial Suite 844 North Patriciaburgh, MH 13975',6,3,2,'FALSE'),
+
+('alec27@bell.com','895-224-8246x9667','cq5wya63n1w79o0hlnpm','128.61.46.131','6174 Nicholas Cliffs Suite 855 Websterview, MN 44283-0599',6,4,2,'FALSE'),
+
+('juliecook@hotmail.com','1-412-734-7967x67388','sj72s0k7wxdrxu3rcmba','61ba:a72b:5401:db1a:9833:d0da:254c:2369','1750 Alicia Shoals Apt. 000 South Benjamin, ND 75373',2,2,1,'FALSE'),
+
+('meganberry@clark.biz','1-991-846-6271x54037','2vpovzopjoqppeqfa5nw','73.182.31.59','34944 Glenn Mountain Sherryhaven, TN 36252',5,4,2,'FALSE'),
+
+('psantiago@nelson.net','885.682.0863','qx6nhdabeaj329zdri0o','81.13.103.118','238 Reeves Villages Leslieton, SD 12318-4476',1,1,1,'FALSE'),
+
+('harrisnicholas@mitchell-hancock.com','(922)411-5121x98410','y3m3px7h46gor851y1pu','58.13.30.104','357 Barbara Crossroad Jacobsburgh, WY 09289-9170',6,4,2,'FALSE'),
+
+('davismike@hotmail.com','467-222-0933x910','4hzaaor6es3iyuiavyio','162.48.39.230','07078 Brent Ways Apt. 999 Alvarezshire, ND 82309',4,3,1,'FALSE'),
+
+('guerramichael@hotmail.com','299-883-0904','webmbgi39hwejv3d90kt','245.38.171.222','13553 Michelle Freeway Apt. 290 Tammyhaven, GU 02869-4630',0,0,1,'TRUE'),
+
+('natalie98@yahoo.com','02934317865','x5ts382m7jezato91fjz','89e:31aa:e9e2:90ea:aa00:4a8c:805c:20ea','23831 Adams Tunnel Suite 538 North David, NE 92200-7011',1,1,1,'FALSE'),
+
+('karen54@mullins.biz','1-019-956-8013','ybz490o0v5781808jtzw','61.219.235.129','526 Ronald Cliffs West Christinaville, GU 76418',8,5,2,'FALSE'),
+
+('avaldez@gmail.com','203-573-2041','bwgvb5w8dm6upvi7ucqc','46.130.37.206','304 Heather Fall East Christina, UT 45880-4522',2,1,1,'FALSE'),
+
+('blackjoel@wright.com','621.591.0560','smxfahki83m4rgsvgvki','9717:f390:29c6:8d60:aee7:a054:1b90:9493','Unit 0643 Box 8346 DPO AA 55632-6780',6,5,1,'FALSE'),
+
+('abigail08@yahoo.com','650-489-7595x7901','0g6v7n9n8k4pekxpqj2e','72.251.102.162','3764 Stanley Hollow East Catherine, CT 98470',5,3,2,'FALSE'),
+
+('catherine64@gmail.com','563-396-2038x288','myjxxvd3jfn1eymfy50n','1289:d2a9:c65d:8fc7:7be4:129b:d7b:cf7b','372 Michael Club Suite 553 Herrerafort, MH 52953',5,4,6,'TRUE');
+
+INSERT INTO transactions VALUES
+('josephhoward@yahoo.com','a9lcj51r','vjbdvd','wt07xm68b',1,'card','JCB 16 digit',18,0,'pending'),
+
+('josephhoward@yahoo.com','y4wcv03i','yp6x27','wt07xm68b',1,'card','JCB 16 digit',26,0,'fulfilled'),
+
+('evansjeffery@yahoo.com','5mi94sfw','nlghpa','41ug157xz',0,'bitcoin','American Express',45,0,'fulfilled'),
+
+('evansjeffery@yahoo.com','br8ba1nu','uw0eeb','41ug157xz',0,'bitcoin','American Express',23,0,'fulfilled'),
+
+('evansjeffery@yahoo.com','a33145ss','bn44oh','y3xp697jx',1,'bitcoin','VISA 16 digit',43,1,'fulfilled'),
+
+('andersonwilliam@yahoo.com','q3lyvbza','yk34y2','8pneoi03z',0,'card','JCB 16 digit',33,0,'failed'),
+
+('andersonwilliam@yahoo.com','3alf9irw','28v6b0','8pneoi03z',0,'card','JCB 16 digit',24,1,'fulfilled'),
+
+('andersonwilliam@yahoo.com','69w9wymx','28v6b0','8pneoi03z',0,'card','JCB 16 digit',24,0,'fulfilled'),
+
+('andersonwilliam@yahoo.com','pegqc4ud','itnk8y','dluy3jka0',0,'card','Discover',25,1,'fulfilled'),
+
+('andersonwilliam@yahoo.com','uo3slwh7','itnk8y','8pneoi03z',0,'card','JCB 16 digit',25,0,'fulfilled'),
+
+('rubenjuarez@yahoo.com','vx4cjc27','fbz9ep','pdxjdwui4',0,'card','Voyager',34,0,'fulfilled'),
+
+('rubenjuarez@yahoo.com','yiay9b0n','7odnn6','pdxjdwui4',0,'card','Voyager',26,0,'fulfilled'),
+
+('rubenjuarez@yahoo.com','sn3v0ql2','audx7o','pdxjdwui4',0,'card','Voyager',25,0,'fulfilled'),
+
+('uchen@malone.com','q8j3dgni','56h7iw','olr4zf09x',0,'card','VISA 13 digit',71,0,'fulfilled'),
+
+('uchen@malone.com','xjulqy8x','kowj1v','gehl28i8u',0,'card','JCB 16 digit',66,1,'fulfilled'),
+
+('uchen@malone.com','rvm65vr1','wlznfu','gehl28i8u',0,'card','JCB 16 digit',69,0,'failed'),
+
+('uchen@malone.com','kckrvagp','hm3rth','76bw9eqg2',0,'apple pay','VISA 16 digit',46,1,'fulfilled'),
+
+('uchen@malone.com','2slep8t9','vvq05w','s6escw45g',0,'card','JCB 16 digit',68,0,'fulfilled'),
+
+('uchen@malone.com','yd80pfko','xwo0al','76bw9eqg2',0,'apple pay','VISA 16 digit',59,0,'fulfilled'),
+
+('uchen@malone.com','ckk3ux58','2528x8','w1i1zq3rg',0,'apple pay','American Express',32,0,'fulfilled');
